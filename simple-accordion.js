de56e31a31b5a92815b77480b4c8582aaac8b6fb
Brandon Beeler <bbeeler@powercreative.com>
2014-08-13 16:49:14 -0400

    first commit

---
+++ simple-accordion.js
@@ -0,0 +1,27 @@
+/**
+ * Accordion Class Definition
+ *   create a simple 2-level expandable accordion
+ *
+ * @author Brandon Beeler
+ */
+var SimpleAccordion = function (selector) {
+    this.$obj = $(selector);
+    this.init();
+}
+
+SimpleAccordion.prototype = {
+    /**
+     * Main Render Function
+     */
+    init: function() {
+        // rig up group headers to toggle collapse classes
+        this.$obj.find(".accordion-group > .group-header, .accordion-sub-group > .sub-group-header").click(function() {
+            var $group = $(this).parent();
+
+            // close other groups before opening a new one
+            // $(".accordion-group").not('.collapsed').not('#' + $group.attr('id')).addClass('collapsed');
+            console.log($group);
+            $group.toggleClass('collapsed');
+        });
+    }
+};
